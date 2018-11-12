c9bd43d29248ae74d64e262aa751731afecb7311
Nonoka Homma <e185730@ie.u-ryukyu.ac.jp>
2018-11-13 00:30:20 +0900

    finish step2

---
+++ src/main/jp/ac/uryukyu/ie/e185730/LivingThing.java
@@ -0,0 +1,42 @@
+package jp.ac.uryukyu.ie.e185730;
+
+public class LivingThing {
+    String name;
+    int hitPoint;
+    int attack;
+    boolean dead;
+
+    public LivingThing(String names, int hitPoints, int attacks){
+        name = names;
+        hitPoint = hitPoints;
+        attack = attacks;
+        dead = false;
+    }
+
+    public boolean isDead(){
+        return  dead;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void attack(LivingThing opponent){
+        if (dead == false){
+            int damage = (int)(Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint <= 0 ) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+
+    }
+
+}
+
