56ffd1b055400b94b3be42aa35a02703390c374a
Nonoka Homma <e185730@ie.u-ryukyu.ac.jp>
2018-11-12 19:42:41 +0900

    testを作った

--- src/test/jp/ac/uryukyu/ie/e185730/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e185730/EnemyTest.java
@@ -1,4 +1,22 @@
 package jp.ac.uryukyu.ie.e185730;
 
-public class EnemyTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+    @Test
+    public void attack() {
+        int heroHP = 10;
+        Hero hero = new Hero("テスト勇者", heroHP, 5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
+        enemy.dead = true;
+        while (enemy.hitPoint>0) {
+            hero.attack(enemy); //乱数で0ダメージとなることもあるため、複数回実行してみる// 。
+        }
+        for (int i = 0; i<5; i++) {
+            enemy.attack(hero);
+        }
+        assertEquals(heroHP, hero.hitPoint);
+    }
 }
