64617293fed39343cb46ee20a31e73606684d0fa
Nonoka Homma <e185730@ie.u-ryukyu.ac.jp>
2018-11-14 15:20:50 +0900

    EnemyTestの間違えの修正とstep3

--- src/test/jp/ac/uryukyu/ie/e185730/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e185730/EnemyTest.java
@@ -10,12 +10,10 @@ class EnemyTest {
         int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
-        while (enemy.hitPoint>0) {
-            hero.attack(enemy); //乱数で0ダメージとなることもあるため、複数回実行してみる// 。
-        }
-        for (int i = 0; i<5; i++) {
+        enemy.setDead(true);
+        for (int i = 0; i<10; i++) {
             enemy.attack(hero);
         }
-        assertEquals(heroHP, hero.hitPoint);
+        assertEquals(heroHP, hero.getHitPoint());
     }
 }
